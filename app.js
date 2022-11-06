40e8450550811117b50af76b33cf0d1fd915c237
Aritra Muhuri <93440930+Aritra37@users.noreply.github.com>
2022-11-06 11:52:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const sign_in_btn =document.querySelector('#sign-in-button');
 const sign_up_btn =document.querySelector('#sign-up-button');
-const container=document.querySelector('.container');
+const page=document.querySelector('.page');
 
 sign_up_btn.addEventListener('click',()=>{
-    container.classList.add('sign-up-mode');
+    page.classList.add('sign-up-mode');
 
 });
 sign_in_btn.addEventListener('click',()=>{
-    container.classList.remove('sign-up-mode');
+    page.classList.remove('sign-up-mode');
 
 });
-
